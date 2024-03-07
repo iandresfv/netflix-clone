9333a9c348221eb302e1c92602fc16ffcb324f63
ignacio <ignacio.farias.v@usach.cl>
2024-03-07 19:46:29 -0300

    feature(verce-style-guide): add vercel style guide and prettier file

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,27 +1,27 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import GoogleIcon from "/public/google.svg";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { GithubIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import GoogleIcon from '/public/google.svg';
 
 export default function Login() {
   return (
-    <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+    <div className="mt-24 rounded bg-black/80 px-6 py-10 md:mt-0 md:max-w-sm md:px-14">
       <form>
         <h1 className="text-3xl font-semibold text-white">Sign In</h1>
-        <div className="space-y-4 mt-5">
+        <div className="mt-5 space-y-4">
           <Input
             type="email"
             name="email"
             placeholder="Email"
-            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+            className="inline-block w-full bg-[#333] placeholder:text-xs placeholder:text-gray-400"
           />
           <Input
             type="password"
             name="password"
             placeholder="Password"
-            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+            className="inline-block w-full bg-[#333] placeholder:text-xs placeholder:text-gray-400"
           />
           <Button
             type="submit"
@@ -33,19 +33,19 @@ export default function Login() {
         </div>
       </form>
 
-      <div className="text-gray-500 text-sm mt-2">
-        New to Netflix?{" "}
+      <div className="mt-2 text-sm text-gray-500">
+        New to Netflix?{' '}
         <Link href="/sign-up" className="text-white hover:underline">
           Sign up now.
         </Link>
       </div>
 
-      <div className="flex w-full justify-center items-center gap-x-3 mt-6">
+      <div className="mt-6 flex w-full items-center justify-center gap-x-3">
         <Button variant="outline" size="icon">
-          <GithubIcon className="w-4 h-4" />
+          <GithubIcon className="h-4 w-4" />
         </Button>
         <Button variant="outline" size="icon">
-          <Image src={GoogleIcon} alt="Google Icon" className="w-6 h-6" />
+          <Image src={GoogleIcon} alt="Google Icon" className="h-6 w-6" />
         </Button>
       </div>
     </div>
